c13af2222f015461572b589b98e7f59bb32ff734
Tanh1625 <dotuananh1625@gmail.com>
2025-02-19 21:52:01 +0700

    Update DBContext.java

--- src/java/dal/DBContext.java
+++ src/java/dal/DBContext.java
@@ -20,7 +20,7 @@ public class DBContext {
             String user = "sa";
             String pass = "manhhuy2104sql";
             String url = "jdbc:sqlserver://localhost:1433;databaseName=DataShop";
-            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDrive");
             connection = DriverManager.getConnection(url, user, pass);
         } catch (ClassNotFoundException | SQLException ex) {
             Logger.getLogger(DBContext.class.getName()).log(Level.SEVERE, null, ex);
