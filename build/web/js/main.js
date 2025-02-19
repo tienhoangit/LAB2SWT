ac16f5e28403fe274f97a5c1f25a66efa29733a4
Tiepnnk <jurgenklopp047@gmail.com>
2025-02-20 03:27:01 +0700

    Update main.js

--- build/web/js/main.js
+++ build/web/js/main.js
@@ -29,7 +29,7 @@
 
     function validate (input) {
         if($(input).attr('type') == 'email' || $(input).attr('name') == 'email') {
-            if($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
+            if($(input).val().trim().match(/^([a-zA-Z\d_\-\.]+)@((\[\d{1,3}\.\d{1,3}\.\d{1,3}\.)|(([a-zA-Z\d\-]+\.)+))([a-zA-Z]{1,5}|\d{1,3})(\]?)$/) == null) {
                 return false;
             }
         }
